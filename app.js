eeb47d6c3c5c382ab326082f2810ebb0917cf04c
sy730113 <sy730113@gmail.com>
2025-04-17 09:51:29 +0530

    app.js

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path'); // You need to require the path module
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
+// View engine setup
 app.set('view engine', 'ejs');
-app.set('layout', './layouts/main');
+app.set('views', path.join(__dirname, 'views')); // Correct views path
+app.set('layout', 'layouts/main'); // Modified layout path
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
 app.use(express.static("public"));
 
+// Routes
 const routes = require('./server/Routes/recipeRoutes.js');
 app.use("/", routes);
 
