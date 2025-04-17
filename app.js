2a33989a853ff37b7b6fd448eccc8d34ef823868
sy730113 <sy730113@gmail.com>
2025-04-17 09:06:58 +0530

    Prepare for Vercel deployment

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const express = require('express');
-const expressLayouts= require('express-ejs-layouts');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-require('dotenv').config(); // Load .env file
-
-const port = process.env.PORT;
-const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config();
-app.set('view engine','ejs');
-app.set('layout',"./layouts/main");
 
-app.use(bodyParser.urlencoded({ extended:true }));
+const app = express();
+const port = process.env.PORT;
 
-app.use(express.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.set('layout', './layouts/main');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
-
 app.use(express.static("public"));
-const routes=require('./server/Routes/recipeRoutes.js');
-app.use("/",routes);
 
-app.listen(port,()=>{
-    console.log(`Server is running on port http://localhost:${port}`);
-});
+const routes = require('./server/Routes/recipeRoutes.js');
+app.use("/", routes);
+
+module.exports = app;
